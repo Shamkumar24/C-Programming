aa8f9b4b343d9e42639ad5f02b46b0bd6747e015
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-09 21:45:28 +0530

    Update Problem01.c

--- Etalvis-Learning/C-Practice/Assignment01/Problem01.c
+++ Etalvis-Learning/C-Practice/Assignment01/Problem01.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+
+int main ()
+{
+    int num, result;
+
+    printf ("Enter the number:") ;
+    scanf ("%d", &num) ;
+
+    result=num+2;
+    printf ("The Result :%d\n", result) ;
+
+}
 
