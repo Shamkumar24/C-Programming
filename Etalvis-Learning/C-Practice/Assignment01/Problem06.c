b037e3ae2e96dc6279d0f98f1d53cec81d7f1ec7
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-11 19:40:22 +0530

    Add program to get last digit of a number
    Implement a program to find the last digit of a number.

---
+++ Etalvis-Learning/C-Practice/Assignment01/Problem06.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{ Problem06.c
+    int num, result;
+
+    printf ("Enter the number:") ;
+    scanf ("%d", &num) ;
+
+    result=num%10;
+    printf ("The Result :%d\n", result) ;
+}
