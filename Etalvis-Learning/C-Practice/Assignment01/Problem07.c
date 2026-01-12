87f42867483bb134f4b809cb9b94b0e8b668f5ca
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-12 18:22:14 +0530

    Add Problem07.c for division by 100

---
+++ Etalvis-Learning/C-Practice/Assignment01/Problem07.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{
+    int num, result;
+
+    printf ("Enter the number:") ;
+    scanf ("%d", &num) ;
+
+    result=num/100;
+    printf ("The Result :%d\n", result) ;
+}
