c092e7e8ff8dbda24ca0f93339d8620bed7e0ad8
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-13 12:08:15 +0530

    Create Program17.c

---
+++ Etalvis-Learning/C-Practice/Assignment01/Program17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, result;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    result = num-((num/10)%2)*5;
+
+    printf("The Result : %d\n", result);
+
+    return 0;
+}
