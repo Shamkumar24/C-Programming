322d36dfb6c3ac253c79b9f7d35d7ceec37ec333
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-13 11:58:15 +0530

    Add program to extract digits from a number

---
+++ Etalvis-Learning/C-Practice/Assignment01/program15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, ones, tens, hundred, result;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    ones = num%10;
+    tens = (num/10)%10;
+    hundred = num/100;
+
+    result = hundred*100+tens*10+2;
+
+    printf("The ones : %d\n", ones);
+    printf("The tens : %d\n", tens);
+    printf("The hundred : %d\n", hundred);
+    printf("The Result : %d\n", result);
+
+    return 0;
+}
