ae057cf4c02430c0e67d6b2eeb640d856eda32b8
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-14 21:13:02 +0530

    Create Problem07.c

---
+++ Etalvis-Learning/C-Practice/Assignment02/Problem07.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, ones, tens, result;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    ones = num % 10;
+    tens = num / 10;
+
+    result = (ones == tens);
+
+    printf("Result : %d\n", result);
+
+    return 0;
+}
