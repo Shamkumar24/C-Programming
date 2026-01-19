b572cfa46e1b0fcf5ce1480bfcd94740b76beb6e
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-19 20:19:09 +0530

    Create Problem06.c

---
+++ Etalvis-Learning/C-Practice/Assignment05/Problem06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, sum = 0;
+
+    for (i = 11; i <= 20; i++)
+    {
+        if ((i % 2) != 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
