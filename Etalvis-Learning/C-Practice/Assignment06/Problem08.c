b0640ee05c3913821f7165be0b21f02c0cfb870f
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-23 18:41:05 +0530

    Add Problem08.c to find specific two-digit numbers

---
+++ Etalvis-Learning/C-Practice/Assignment06/Problem08.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 11, ones, tens, sum;
+
+    while (i < 100)
+    {
+        ones = i % 10;
+        tens = i / 10;
+        sum = tens + ones;
+
+        if ((i % 2) == 0 && sum == 6)
+            printf("%d\n", i);
+
+        i++;
+    }
+
+    return 0;
+}
