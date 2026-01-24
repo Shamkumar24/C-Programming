c856652c313fc76cb7d793c6a7cd2bd3eab34e45
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-24 15:46:40 +0530

    Add program to calculate the sum of digits of a number

---
+++ Etalvis-Learning/C-Practice/Assignment06/Problem12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0;
+
+    printf("Enter the number : ");
+    scanf("%d", &num);
+
+    while (num > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+
+    printf("Result : %d\n", sum);
+
+    return 0;
+}
