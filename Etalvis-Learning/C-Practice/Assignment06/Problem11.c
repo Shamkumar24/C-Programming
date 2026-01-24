1bd2510aa4c61407efb3d0a5b1b81304addb1b5c
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-24 15:45:34 +0530

    Add Problem11.c to count digits in a number

---
+++ Etalvis-Learning/C-Practice/Assignment06/Problem11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, count = 0;
+
+    printf("Enter the number : ");
+    scanf("%d", &num);
+
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+
+    printf("Result : %d\n", count);
+
+    return 0;
+}
