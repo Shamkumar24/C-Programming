cada5bf7673e69dda00ca62c6167c7360d07cd2b
SHAMKUMAR M <shamk7765@gmail.com>
2026-01-26 17:07:16 +0530

    Create Problem05.c

---
+++ Etalvis-Learning/C-Practice/Assignment07/Problem05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void disp_odd()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i % 2 != 0)
+            printf("%d\n", i);
+    }
+}
+
+int main()
+{
+    disp_odd();
+    return 0;
+}
