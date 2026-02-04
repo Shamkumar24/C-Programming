e158bbd025b5925f2c33f67064467446a5252a65
SHAMKUMAR M <shamk7765@gmail.com>
2026-02-04 17:03:16 +0530

    Create problem12.c

---
+++ Etalvis-Learning/C-Practice/Assignment08/problem12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int disp(int num)
+{
+    int count = 0;
+
+    while (num > 10)
+    {
+        if ((num % 100) % 2 != 0)
+        {
+            count++;
+        }
+        num = num / 10;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int x, result;
+
+    printf("Enter the number : ");
+    scanf("%d", &x);
+
+    result = disp(x);
+    printf("Result : %d\n", result);
+
+    return 0;
+}
